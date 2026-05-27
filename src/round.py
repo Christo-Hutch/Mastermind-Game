f32e695558afc8f4817c75385e327824248e3763
Christo-Hutch <chris.hutchinson008@gmail.com>
2026-05-27 23:41:18 +0100

    feat(round): in/decrement for row variables

--- src/round.py
+++ src/round.py
@@ -58,6 +58,8 @@ class Round:
             raise ValueError("No remaining rows for board left!")
         
         self.board.change_row(row_num, row_data)
+        self.first_empty_row += 1
+        self.remaining_rows -= 1
         response = self.calculate_response(row_data)
         self.responses[row_num] = response
 
