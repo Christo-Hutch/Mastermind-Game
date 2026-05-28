e79ec0d5006fb6b60411fcd1dd3300e6866cfe41
Christo-Hutch <chris.hutchinson008@gmail.com>
2026-05-28 13:33:20 +0100

    docs(board): edit comment explaining class purpose

--- src/board.py
+++ src/board.py
@@ -18,7 +18,7 @@ terminal_colors = {
 This class is used to create, manipulate and show the 2D array used to
 store the values within a board. At the beginning of every round, a new
 instance of a Board object is created and then discarded at the end.
-Code-maker responses are handled by a separate class, this class only
+Code-maker responses are handled by the round class, this class only
 handles the code-breaker attempts.
 """
 class Board:
