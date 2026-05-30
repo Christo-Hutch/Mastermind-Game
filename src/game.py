97488ad151e6d2d778cb6e7b16b40436bc9fe73f
Christo-Hutch <chris.hutchinson008@gmail.com>
2026-05-30 17:38:12 +0100

    feat(game): add Game class
    Add a 'Game' class which is used to run all the methods within the round
    class in the correct order/manner. This class features methods for
    multiple rounds within a single game and tracking of a total score
    across all rounds. As of this commit, the following methods have been
    added:
    - Play a single round with variable round_num and board dimensions.
    - Start a game with a variable number of rounds.

---
+++ src/game.py
@@ -0,0 +1,57 @@
+from round import Round
+from board import Board
+from utils import get_colors_tooltip, clean_user_input, clear_terminal
+
+class Game:
+    def __init__(self, round_count: int, rounds_config: list):
+        if len(rounds_config) != round_count:
+            raise ValueError(f"'rounds_config' doesn't contain {round_count} dictionary configs")
+        
+        """
+        Rounds Config is a list of dictionaries, the index of the
+        dictionary correlates to the round and the desired config.
+        """
+        self.rounds_config: list = rounds_config
+        self.round_count: int = round_count
+        self.total_score: float = 0.0
+
+    def play_round(self, round_num: int, row_count: int, column_count: int):
+        new_board = Board(row_count, column_count)
+        new_round = Round(new_board)
+        colors = new_board.colors.copy()
+        del colors["  "]
+
+        while new_round.remaining_rows > 0 and not new_round.isWon:
+            clear_terminal()
+            
+            print(f"---ROUND {round_num}---")
+            new_round.print_round_board()
+            print(f"You can enter {new_board.num_of_columns} colors:\n" + get_colors_tooltip(colors))
+
+            valid_user_input = False
+            clean_input = []
+            
+            while not valid_user_input:
+                user_input = input("Enter your colors: ")
+                clean_input = clean_user_input(user_input, colors)
+
+                if len(clean_input) == new_board.num_of_columns:
+                    valid_user_input = True
+
+            new_round.attempt_row(clean_input)
+
+        clear_terminal()
+        new_round.print_round_board()
+
+        if new_round.isWon:
+            round_score = new_round.get_score()
+            self.total_score += round_score
+            print(f"Congratulations! You won! Score: {round_score}")
+        else:
+            print(f"Game Over! You ran out of turns. The code was: {' '.join(new_round.code)}")
+
+        print(f"Total Game Score: {self.total_score}")
+
+    def play_game(self):
+        for round_num in range(1, self.round_count + 1):
+            self.play_round(round_num, self.rounds_config[round_num - 1]["rows"], self.rounds_config[round_num - 1]["columns"])
