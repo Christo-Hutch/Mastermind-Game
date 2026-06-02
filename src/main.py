53b1392fc7b21e1c1aec7a2b1fc508895b838f27
Christo-Hutch <chris.hutchinson008@gmail.com>
2026-06-02 17:38:29 +0100

    feat: edit main file to call main menu method

--- src/main.py
+++ src/main.py
@@ -1,5 +1,3 @@
-from game import Game
+from navi import MenuNavi
 
-game_one = Game(3, [{"rows": 10, "columns": 4}, {"rows": 8, "columns": 4}, {"rows": 6, "columns": 4}])
-
-game_one.play_game()
+MenuNavi.main_menu()
